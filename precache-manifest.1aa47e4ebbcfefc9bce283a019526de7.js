8cb55909c98e1bf267846fbba1e7ed39ee0c53b3
Siamak <siamak.rp@gmail.com>
2020-08-03 23:53:34 +0800

    Updates

---
+++ precache-manifest.1aa47e4ebbcfefc9bce283a019526de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a490d59fc85a710ea159246a895972f",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "c45a25111663ea05f390",
+    "url": "/weather/static/css/main.366b2edf.chunk.css"
+  },
+  {
+    "revision": "5ade6af2439239d0218c",
+    "url": "/weather/static/js/2.174a2a21.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/weather/static/js/2.174a2a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c45a25111663ea05f390",
+    "url": "/weather/static/js/main.f017eb94.chunk.js"
+  },
+  {
+    "revision": "e741918522270997b492",
+    "url": "/weather/static/js/runtime-main.3b601d73.js"
+  }
+]);
